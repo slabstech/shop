cfdc7d4dbdad6ac88cc9efe226bd07d2cdd631fc
sachin <sachins911991@gmail.com>
2022-06-08 08:46:04 +0200

    nextpart: modifying parameters to setup project Shop

--- clients/reactjs/shop/src/App.js
+++ clients/reactjs/shop/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <br/>
+      <br/>
+
+      Shop Web App - Under Construction
+      <br/>
+      <br/>
+
+      Built using <a href="https://github.com/sachinsshetty/revive">Revive</a>
+      <br/>
+      <br/>
+
+      Source code available at <a href="https://github.com/slabstech/shop">Shop</a>
     </div>
   );
 }
--- docker/postgresql_schema.sql
+++ docker/postgresql_schema.sql
@@ -1,11 +1,3 @@
-create table setup(
-id  serial  primary key,
-type_name varchar(255),
-option_value varchar(255),
-use_case varchar(255),
-status int,
-version int);
-
 create table configuration(
 id serial primary key,
 key varchar(255),
